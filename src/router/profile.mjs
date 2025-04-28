42677ddd4864411c6e505214960077edc94c9cbe
SSumu <sadeepalsumudu@gmail.com>
2025-04-28 10:26:51 +0530

    Pushing the saved changes of the profile.mjs

--- src/router/profile.mjs
+++ src/router/profile.mjs
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { comQValidate, comValidate } from "../utils/validatorMethod.mjs";
 import { matchedData, validationResult } from "express-validator";
 import { resError } from "../utils/error-creator.mjs";
+import DB from "../db/db.mjs";
 
 const profileRouter = Router();
 
@@ -14,7 +15,7 @@ profileRouter.post(
   "/create",
   comQValidate("userId"),
   comValidate("Image"),
-  (req, res) => {
+  async (req, res) => {
     const error = validationResult(req);
     const err = /*loginError*/ resError(error.array()); // We can use middleware if we need.
 
@@ -29,6 +30,26 @@ profileRouter.post(
     const data = matchedData(req); // This will give us the data which we want. This will give the real data if the data has come correcly.Then we can make it to put it into the database.
     // we can see what is include in the data if we want.
     console.log(data);
+
+    // In here, this is the creation of the profile.
+    try {
+      // First we have to migrate profile. Then it will be suggested at here.
+      await DB.profile.create({
+        // data is necessarily have in here. userId necessarily has in here.
+        data: {
+          UserId: data.userId,
+          Image: data.Image,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res.status(500).json({
+        msg: "error",
+        error: "database error",
+        data: null,
+      });
+    }
 
     res.sendStatus(200);
 
