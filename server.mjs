887e7c7ed96029cacbb337ff535d75209aaf4e7c
SSumu <sadeepalsumudu@gmail.com>
2025-07-13 14:44:44 +0530

    Mastering Cookies and Sessions

--- server.mjs
+++ server.mjs
@@ -1,12 +1,40 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import expressSession from "express-session";
 // import routers
 // import userRouter from "./src/router/user.mjs";
 // import productRouter from "./src/router/product.mjs";
 import rootRouter from "./src/router/index.mjs";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import DB from "./src/db/db.mjs";
 
 const server = express();
 
 server.use(express.json());
+server.use(cookieParser("mykey")); // Register the cookie-parser as a middleware. Inside the cookieParser(), there are parameters as secret. This is to join the cookieParser. There it can put key as a secret key for secret parameter. options parameter are optional. The important thing is putting the key and what is the advantage and disadvantage of it and what happens from it. The incident happens there is creating a sign cookie. sign cookie is putting(applying) a signature to it. Simple opinion of sign cookie is putting a small code to cookie.
+server.use(
+  expressSession({
+    secret: "mykey", // In here the secret key is same as in the secret key in the cookies.
+    resave: false, // When we call the session every time, it saves the session even the session is exist or not. Most times it should be keep as false. It saves the session even if the changes happen or not in the session. But it should not be happened. So that is why we keep it false.
+    saveUninitialized: true, // if it not initialized, then it is not saved. When we keep this to false, at each call Previous Session is "immediately" changed to the New Session. It is a unnecessary thing because if already a session is initialized, then there is no need to initialize a new session. If a session is Uninitialized, then it needs to be saved. So that is why we keep it as true. There it is not practical that to put a New Session ID when every times the Sessions are created.
+
+    // In the Session, this cookie keeps the data set. (Even if the Cookie keeps string in it.)
+    cookie: {
+      maxAge: 1000 * 60, // Expiration time. How long the cookie will exist?
+      httpOnly: true,
+      signed: true,
+    }, // Small description about the session goes to client side as a cookie. Those details must be given inside the brackets. Those details are the things related to the expiration of the session.
+
+    // prisma-session-store is installed in here after the cookies.
+    // As we already have a prisma client, we do not need a new prisma client.
+    // prisma client can be brought to here from db method.
+    store: new PrismaSessionStore(DB, {
+      checkPeriod: 2 * 60 * 1000, //ms (2 is minutes. Function of the checkPeriod is to check sessions per the given time(2 * 60 * 1000 ms = 2 minutes) and if the sessions were expired then remove those expired sessions.)
+      dbRecordIdIsSessionId: true, // This says that use the sid in the database as session id.
+      dbRecordIdFunction: undefined, // This says about session id generate function. But in here, it does not need as it is already in here. Because it is automatically generated.
+    }), // First one is DB because we have initialized it. Second one is the parameters which are inside the curly brackets.
+  }) // Session's basic facts(options) are prepared.
+);
 // server.use("/api/v2/", rootRouter);
 server.use("/api/v1", rootRouter);
 // server.use("/api/v1/user", userRouter);
--- src/router/test1.mjs
+++ src/router/test1.mjs
@@ -14,106 +14,161 @@ const testRouter1 = Router();
 
 // Middleware is a request response chain.
 // testRouter.get("/", (req, res) => res.sendStatus(200),(req, res) => res.sendStatus(200));
-testRouter1.get(
-  // Using query
-  // "/abc",
-  // query("name")
-  //   .isEmail()
-  //   .withMessage("not a email")
-  //   .isBoolean()
-  //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
-  // query("age").isNumeric().withMessage("not a number"), // There it can be added further more queries by using middleware. This is a middleware.
+// testRouter1.get(
+//   // Using query
+//   // "/abc",
+//   // query("name")
+//   //   .isEmail()
+//   //   .withMessage("not a email")
+//   //   .isBoolean()
+//   //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
+//   // query("age").isNumeric().withMessage("not a number"), // There it can be added further more queries by using middleware. This is a middleware.
 
-  // Using path param
-  // "/abc/:name/:age", // : This is to pick the param
-  // param("name")
-  //   .isEmail()
-  //   .withMessage("not a email")
-  //   .isBoolean()
-  //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
-  // param("age").isNumeric().withMessage("not a number"),
+//   // Using path param
+//   // "/abc/:name/:age", // : This is to pick the param
+//   // param("name")
+//   //   .isEmail()
+//   //   .withMessage("not a email")
+//   //   .isBoolean()
+//   //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
+//   // param("age").isNumeric().withMessage("not a number"),
 
-  // Using body
-  // "/abc",
-  // body("name")
-  //   .isEmail()
-  //   .withMessage("not a email")
-  //   .isBoolean()
-  //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
-  // body("age").isNumeric().withMessage("not a number"),
+//   // Using body
+//   // "/abc",
+//   // body("name")
+//   //   .isEmail()
+//   //   .withMessage("not a email")
+//   //   .isBoolean()
+//   //   .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
+//   // body("age").isNumeric().withMessage("not a number"),
 
-  // "/abc",
-  // [
-  //   body("name")
-  //     .isEmail()
-  //     .withMessage("not a email")
-  //     .isBoolean()
-  //     .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
-  //   body("age").isNumeric().withMessage("not a number"),
-  // ],
+//   // "/abc",
+//   // [
+//   //   body("name")
+//   //     .isEmail()
+//   //     .withMessage("not a email")
+//   //     .isBoolean()
+//   //     .withMessage("not a bool") /*isString() isEmpty() isEmail()*/, // withMessage() method allows us to send a error message which we want. This can be prolong as a chain.
+//   //   body("age").isNumeric().withMessage("not a number"),
+//   // ],
 
-  // "/abc",
-  // validatorMethod(),
-  // (req, res) => {
-  //   // isString means that in which way query('name') should be exist.
-  //   //   console.log(req.query?.name);
-  //   //   console.log(req["express-validator#contexts"]); // This is where the place of the details of express-validator
-  //   const r = validationResult(req); // This is a function from express-validator to form and prepare the details in pleasent way and then take that details in a regular way. Put the request from express-validation.
-  //   const d = matchedData(req); // This method will only give the correct data for the process.
+//   // "/abc",
+//   // validatorMethod(),
+//   // (req, res) => {
+//   //   // isString means that in which way query('name') should be exist.
+//   //   //   console.log(req.query?.name);
+//   //   //   console.log(req["express-validator#contexts"]); // This is where the place of the details of express-validator
+//   //   const r = validationResult(req); // This is a function from express-validator to form and prepare the details in pleasent way and then take that details in a regular way. Put the request from express-validation.
+//   //   const d = matchedData(req); // This method will only give the correct data for the process.
 
-  //   // console.log(r);
-  //   // console.log(r.array()); // array() method gives a array as the result in pleasent and regular way.
-  //   console.log(d);
-  //   if (r.array().length) return res.sendStatus(400); // This means if there is a length, there are errors or otherwise if there is not a length there are no errors.
-  //   res.sendStatus(200);
-  // }
+//   //   // console.log(r);
+//   //   // console.log(r.array()); // array() method gives a array as the result in pleasent and regular way.
+//   //   console.log(d);
+//   //   if (r.array().length) return res.sendStatus(400); // This means if there is a length, there are errors or otherwise if there is not a length there are no errors.
+//   //   res.sendStatus(200);
+//   // }
 
-  // using schema
-  "/abc/:age",
-  checkSchema({
-    // This is to send data as a schema. schema will defaultly check the body for the data. Then it will also check the query and the path param for the data. It can identify the location where the data come from.
-    // name:{
-    //   // Here in this way errorMessage is only for isEmail.
-    //   isEmail:true,
-    //   errorMessage:'not a email'
-    // }
-    age: {
-      isNumeric: {
-        errorMessage: "not a number",
-      },
-    },
-    name: {
-      // This is the way to give errorMessage for unique fields.
-      // Unique field
-      isEmail: {
-        errorMessage: "not a email",
-      },
-      // Unique field
-      isBoolean: {
-        errorMessage: "not a bool",
-      },
-    },
-    city: {
-      // This is the common way to give errorMessage for all the fields.
-      isNumeric: true,
-      errorMessage: "not a valid city",
-    },
-  }),
-  (req, res) => {
-    // isString means that in which way query('name') should be exist.
-    //   console.log(req.query?.name);
-    //   console.log(req["express-validator#contexts"]); // This is where the place of the details of express-validator
-    const r = validationResult(req); // This is a function from express-validator to format and prepare the details in pleasent way and then take that details in a regular way. Put the request from express-validation.
-    const d = matchedData(req); // This method will only give the correct data for the process.
+//   // using schema
+//   "/abc/:age",
+//   checkSchema({
+//     // This is to send data as a schema. schema will defaultly check the body for the data. Then it will also check the query and the path param for the data. It can identify the location where the data come from.
+//     // name:{
+//     //   // Here in this way errorMessage is only for isEmail.
+//     //   isEmail:true,
+//     //   errorMessage:'not a email'
+//     // }
+//     age: {
+//       isNumeric: {
+//         errorMessage: "not a number",
+//       },
+//     },
+//     name: {
+//       // This is the way to give errorMessage for unique fields.
+//       // Unique field
+//       isEmail: {
+//         errorMessage: "not a email",
+//       },
+//       // Unique field
+//       isBoolean: {
+//         errorMessage: "not a bool",
+//       },
+//     },
+//     city: {
+//       // This is the common way to give errorMessage for all the fields.
+//       isNumeric: true,
+//       errorMessage: "not a valid city",
+//     },
+//   }),
+//   (req, res) => {
+//     // isString means that in which way query('name') should be exist.
+//     //   console.log(req.query?.name);
+//     //   console.log(req["express-validator#contexts"]); // This is where the place of the details of express-validator
+//     const r = validationResult(req); // This is a function from express-validator to format and prepare the details in pleasent way and then take that details in a regular way. Put the request from express-validation.
+//     const d = matchedData(req); // This method will only give the correct data for the process.
 
-    // console.log(r);
-    // console.log(r.array()); // array() method gives a array as the result in pleasent and regular way.
+//     // console.log(r);
+//     // console.log(r.array()); // array() method gives a array as the result in pleasent and regular way.
 
-    // console.log(d);
-    console.log(r.array());
-    if (r.array().length) return res.sendStatus(400); // This means if there is a length, there are errors or otherwise if there is not a length there are no errors.
-    res.sendStatus(200);
-  }
-);
+//     // console.log(d);
+//     console.log(r.array());
+//     if (r.array().length) return res.sendStatus(400); // This means if there is a length, there are errors or otherwise if there is not a length there are no errors.
+//     res.sendStatus(200);
+//   }
+// );
+
+testRouter1.get("/get-cookie", (req, res) => {
+  // Let's send a cookie to the client side as a response. We can add more cookies.
+  // This is the registration of cookies. (Cookies's registering)
+  res.cookie("sadeepal-cookie", "sadeepal sumudupriya", {
+    maxAge: 1000 * 30, // maxAge is the main thing in the option. This means that how much time it takes to automatically expire the cookie. It must be given in milliseconds
+    httpOnly: true, // The simple meaning of httpOnly is cookie is accessed (or can be accessed) through the http requests. This is to prevent the cookie from being accessed from the client side.
+    signed: true, // This cookie is a signed cookie.
+  }); // Parameters are name: string, val: string, options: CookieOptions
+  res.cookie("sadeepal-cookie-2", "sadeepal", {
+    maxAge: 1000 * 60, // maxAge is the main thing in the option. This means that how much time it takes to automatically expire the cookie. It must be given in milliseconds.
+    httpOnly: true, // The simple meaning of httpOnly is cookie is accessed (or can be accessed) through the http requests. This is another option in the cookie and it is most important option. Even we give the httpOnly: true, ,cyber security persons know whether the cookie is accessible or not.
+  }); // Parameters are name: string, val: string, options: CookieOptions
+  // It is difficult to access a cookie through inbuilt JavaScript. It is not cannot but difficult.
+  res.sendStatus(200); // Return response
+});
+
+// To check the cookie. This is to read the cookie in the server side. For that this end-point needs to do that. This end-point can be a any end-point.
+testRouter1.get("/read-cookie", (req, res) => {
+  // These two console.logs have two sides. We have to check which one get the response. It means that which one reads the cookie.
+  console.log(req.cookies); // Read the cookies. We can directly access like this. From this we can check whether the data are there or not. Also we can check the validation. So its clearly see that the cookie we have put in here has been registered to the port related to the domain (localhost in here).
+  console.log(req.signedCookies["sadeepal-cookie"]); // Now the sadeepal-cookie must be read in a different way. signedCookies are accessed like this. ['sadeepal-cookie'] means accessing the sadeepal-cookie cookie.
+  // console.log(req.headers.cookie); We can also access this from the header.
+
+  res.sendStatus(200);
+});
+
+// We have to register a session like above
+testRouter1.get("/get-session", (req, res) => {
+  // Session registration is different. This is the session registration.
+  // Here the session registration is written to the request.
+  // This is the writting of a session. But it belongs to the request.
+  req.session["sadeepal-session"] = {
+    // Data
+    name: "sadeepal",
+    age: 27,
+  };
+  req.session["sadeepal-session-2"] = {
+    // Data
+    name: "sumudupriya",
+    age: 27,
+  };
+  // Same end-point is used for the both sessions.
+  res.sendStatus(200);
+});
+
+testRouter1.get("/read-session", (req, res) => {
+  console.log(req.sessionID);
+
+  console.log(req.session);
+
+  res.sendStatus(200);
+});
+
+// Both session registration and getting are done by the request.
 
 export default testRouter1;
